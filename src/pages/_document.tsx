f1b271c222f9033e0b0d0fae0a455307ab1e84e1
aran <aranabdi@yahoo.com>
2023-04-13 10:00:10 +0330

    Supportation of internationalisation, rtl, Iran-Sans font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,9 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript, DocumentProps } from 'next/document'
 
-export default function Document() {
+export default function Document(props: DocumentProps) {
+
   return (
-    <Html lang="en">
+    <Html lang={props.locale} dir={props.locale === 'fa' ? 'rtl': 'ltr'}>
       <Head />
       <body>
         <Main />
