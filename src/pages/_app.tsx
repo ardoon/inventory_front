ed7406cb7e566da33b5b1020a5eb4aabc3dbaac5
aran <aranabdi@yahoo.com>
2023-04-27 12:51:07 +0330

    Adding Redux-toolkit to project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,17 @@ import '@/styles/globals.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import { appWithTranslation } from 'next-i18next'
 import { AppProps } from 'next/app'
+import { store } from './../store/store'
+import { Provider } from 'react-redux'
 
 function App({ Component, pageProps }: AppProps) {
 
-  return <Component {...pageProps} />
-  
+  return (
+    <Provider store={store} >
+      <Component {...pageProps} />
+    </Provider>
+  )
+
 }
 
 export default appWithTranslation(App)
