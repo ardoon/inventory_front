4d06976b1278c761c391b5a4128acf1a7ecb1b49
aran <aranabdi@yahoo.com>
2023-05-01 19:49:34 +0330

    Categories basic slice and redux config

---
+++ src/store/slices/categoriesSlice.ts
@@ -0,0 +1,29 @@
+import { createSlice } from '@reduxjs/toolkit'
+import type { PayloadAction } from '@reduxjs/toolkit'
+import Category from '@/models/category'
+
+const initialState: Category[] = []
+
+export const categoriesSlice = createSlice({
+  name: 'categories',
+  initialState,
+  reducers: {
+    addCategory : (state, action: PayloadAction<Category>) => {
+      state.push(action.payload)
+    },
+    updateCategory : (state, action: PayloadAction<{category: Partial<Category>, key: string}>) => {
+      const { category, key } = action.payload
+      return state.map((item: Category) => {
+        return item.name === key ? {... item, ...category} : item;
+      })
+    },
+    deleteCategory : (state, action: PayloadAction<string>) => {
+      return state.filter((category: Category) => category.name !== action.payload)
+    }
+  },
+})
+
+// Action creators are generated for each case reducer function
+export const { addCategory, updateCategory, deleteCategory } = categoriesSlice.actions
+
+export default categoriesSlice.reducer
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,15 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit'
 import productsSlice from './slices/productsSlice'
 import unitReducer from './slices/unitsSlice'
+import categoriesReducer from './slices/categoriesSlice'
 
 export const store = configureStore({
   reducer: {
     products: productsSlice,
-    units: unitReducer
+    units: unitReducer,
+    categories: categoriesReducer
   },
 })
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+
 export type AppDispatch = typeof store.dispatch
